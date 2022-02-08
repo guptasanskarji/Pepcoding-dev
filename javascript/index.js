17cb82c78ee8017c1dedf739219260f2bbcae6df
guptasanskarji <guptasanskarsg@gmail.com>
2022-02-08 23:04:16 +0530

    javascript

---
+++ javascript/index.js
@@ -0,0 +1,47 @@
+// javascript (1995) --> frontend + backend + database
+//starting Javascript
+
+// var let const
+
+// Variable declaration
+var a;       // int string float ?
+
+//Variable initialization
+a = 10;
+console.log(typeof a);      // type-of operator --> it tells us the type of data stored in a variable
+console.log(a);
+
+a = 'hello';       // re-initialize of variable 'a'
+console.log(typeof a);
+console.log(a);
+
+a = true;
+console.log(typeof a);
+console.log(a);
+
+a = null;
+console.log(typeof a);
+console.log(a);
+
+//Number
+var num = 10;
+console.log(num); 
+
+//float
+var float = 2.4;
+console.log(float);
+
+//Boolean
+var t = true;
+console.log(t);
+var f = false;
+console.log(f);
+
+//string --> single quotes('') , double quotes("") , backtick(``)
+var str = 'a';
+console.log(str);
+var str = "how r u?"
+console.log(str);
+
+
+
