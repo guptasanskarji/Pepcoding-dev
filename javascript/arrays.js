cad1f4b57884f07ce5c4b102bc3c1462584b3ecc
guptasanskarji <guptasanskarsg@gmail.com>
2022-02-12 23:49:11 +0530

    Javascript

---
+++ javascript/arrays.js
@@ -0,0 +1,49 @@
+/////////        ARRAYS (12/02/2022) :- It is a collection of elements       ////////
+
+let cars = ["BMW","AUDI", "MG", 1, 2, 3, true];
+//in Js u can store different values of different types in array
+console.log(cars);
+
+// accessing the elements in an array
+console.log(cars[0]);
+console.log(cars[3]);
+
+// Replacing elements in an array
+cars[3] = "Mahindra";
+console.log(cars[3]);
+
+//adding element in an array 
+cars[7]="honda";
+console.log(cars);
+
+// Methods of an array
+
+// 1). pop method :-> this method removes the element from last of array
+cars.pop();
+console.log("after popping the elements\n" + cars);
+
+// 2). push method :--> it pushes the new element at the end of an array
+cars.push("TATA");
+console.log("after pushing an element");
+console.log(cars);
+
+// 3). unshift method :--> it add element at the starting of an array
+cars.unshift("JLR");
+cars.unshift("Mustang");
+console.log(cars);
+
+// 4). shift method :--> it removes element from 0th index of element
+cars.shift();
+cars.shift();
+console.log(cars);
+
+/////////       2D ARRAY              //////
+
+let array2d = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(array2d);
+console.table(array2d);
+console.log(array2d[1][2]);
--- javascript/functions.js
+++ javascript/functions.js
@@ -12,29 +12,46 @@
 // Function invoke :--
       // function_name(arg1, arg2);
 
-function add (a,b){
-    console.log(a + b);
-}
-
-add(2, 6);
-
-//
-//  --> Functions can be returned
-//  --> Function can be passed as parameters/arguments
-
-function calculator(str,a,b){
-    if(str == 'add'){
-         return function add(a,b){
-             console.log( a+b);
-         }
+      function add (a,b){
+        console.log(a + b);
     }
-    else if(str == 'sub'){
-        return function sub(a,b){
-             console.log(a-b);
+    
+    add(2, 6);
+    
+    //
+    //  --> Functions can be returned
+    //  --> Function can be passed as parameters/arguments
+    
+    function calculator(str,a,b){
+        if(str == 'add'){
+             return function add(){
+                 console.log( a + b);
+             }
         }
+        else if(str == 'sub'){
+            return function sub(a,b){
+                 console.log(a-b);
+            }
+        } 
     }
-}
+    
+    let returnedFunc = calculator('add',2,4);
+    console.log("returned function is \n" + returnedFunc);
+    returnedFunc();
 
-let returnedFunc = calculator('add',2,4);
-console.log("returned function is \n" + returnedFunc);
-returnedFunc();
+ // 3). IIFE :- immediately invoke function expression
+ 
+let additionIIFE = (function add(a,b){
+    return a+b;
+})(20,30);
+console.log(""+ additionIIFE);
+
+//       OR
+
+let additionIIFE = (function add(a,b){
+    console.log(a+b);
+})(20,30);
+
+
+
+ 
