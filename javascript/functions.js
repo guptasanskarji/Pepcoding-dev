a6ea17f4939ecc22448acd2653d520ebb760e7e7
guptasanskarji <guptasanskarsg@gmail.com>
2022-02-12 22:39:47 +0530

    Javascript

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,3 +1,40 @@
 //////////////////// 12/02/2022 /////////////////////////////
 
-//   FUNCTIONS   //
+//   FUNCTIONS :- 3 types of function  //
+
+// 1.Normal function
+
+//Function definition :-- 
+       //         function_name(param1, param2){
+       //           do something
+       //         }
+
+// Function invoke :--
+      // function_name(arg1, arg2);
+
+function add (a,b){
+    console.log(a + b);
+}
+
+add(2, 6);
+
+//
+//  --> Functions can be returned
+//  --> Function can be passed as parameters/arguments
+
+function calculator(str,a,b){
+    if(str == 'add'){
+         return function add(a,b){
+             console.log( a+b);
+         }
+    }
+    else if(str == 'sub'){
+        return function sub(a,b){
+             console.log(a-b);
+        }
+    }
+}
+
+let returnedFunc = calculator('add',2,4);
+console.log("returned function is \n" + returnedFunc);
+returnedFunc();
