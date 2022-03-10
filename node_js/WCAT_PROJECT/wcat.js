908b6a1e845e48e918569777ed96275ef84589a5
guptasanskarji <guptasanskarsg@gmail.com>
2022-03-10 23:54:03 +0530

    wcat

--- node_js/WCAT_PROJECT/wcat.js
+++ node_js/WCAT_PROJECT/wcat.js
@@ -24,7 +24,8 @@ for(let i=0;i<filesArr.length;i++){
     let doesExist = fs.existsSync(filesArr[i]);
     if(!doesExist){
         console.log("files doesn't exists"); 
-        return;
+       // return;
+       process.exit();
     }
 }
 
@@ -32,9 +33,88 @@ for(let i=0;i<filesArr.length;i++){
 let content = "";
 for(let i=0;i<filesArr.length;i++){
     let fileContent = fs.readFileSync(filesArr[i]);
-    content+=fileContent+'\n';
+    content+=fileContent+'\r\n';
 }
 console.log(content);
 
-let contentArr =content.split("\n");
+let contentArr =content.split("\r\n");
+console.table(contentArr);
+
+// check if -s is present or not
+
+let isSPresent = optionsArr.includes("-s");
+let tempArr=[];
+if(isSPresent){
+    for(let i=1;i<contentArr.length;i++){
+        if(contentArr[i]=="" && contentArr[i-1]==""){
+            contentArr[i]=null;
+        }
+        else  if(contentArr[i]=="" && contentArr[i-1]==null){
+            contentArr[i]=null;
+  }
+}
+console.table(contentArr);
+
+// push everything in tempArr except null
+for(let i=0;i<contentArr.length;i++){
+    if(contentArr[i]!=null){
+        tempArr.push(contentArr[i]);
+    }
+}
+console.log("data after removing extra lines\n",tempArr);
+}
+
+// check -n and -b
+contentArr = tempArr;
+
+let indexOfN = optionsArr.indexOf("-n");
+let indexOfB = optionsArr.indexOf("-b");
+// if -n and -b is not found then return -1
+
+let finalOption = "";
+
+// if both -n & -b are present
+if(indexOfN != -1 && indexOfB != -1){
+    if(indexOfN < indexOfB){
+        finalOption = "-n";
+    }
+    else{
+        finalOption = "-b";
+    }
+}
+
+// either -n is present or -b is present
+
+else{
+    if(indexOfN != -1){
+        finalOption = "-n";
+    }
+    else if(indexOfB != -1){
+        finalOption = "-b";
+    }
+}
+
+//calling of functions by evaluating finalOption
+if(finalOption == "-n"){
+    modifiyContentByN();
+}
+else if(finalOption == "-b"){
+    modifiyContentByB();
+}
+
+function modifiyContentByN() {
+       for(let i=0;i<contentArr.length;i++){
+           contentArr[i] = (i+1) + ") " + contentArr[i];
+       }
+}
+
+function modifiyContentByB() {
+    let count = 0;
+    for(let i=0;i<contentArr.length;i++){
+        if(contentArr[i] != ""){
+             contentArr[i] = count  + ") " + contentArr[i];
+             count++;
+        }
+    }
+}
 console.log(contentArr);
